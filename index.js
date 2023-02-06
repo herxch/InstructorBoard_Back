dfeaa2b0af95a21c7f5d389980dc1f67bcf8db19
Herbert Xu <herbert@sutongctr.com>
2023-02-06 16:43:57 -0600

    rework

--- index.js
+++ index.js
@@ -3,69 +3,72 @@ import { Server } from 'socket.io';
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
-  pingInterval: 24 * 60 * 60 * 1000,
-    pingTimeout: 3 * 24 * 60 * 60 * 1000,
+  // pingInterval: 24 * 60 * 60 * 1000,
+  //   pingTimeout: 3 * 24 * 60 * 60 * 1000,
   cors: {
     origin: process.env.FRONT_URL,
     credentials: true,
   },
 });
 
-let users = [];
-let answers = [];
+// let users = [];
+// let answers = [];
 
 io.on('connection', (socket) => {
-  // Student join response
-  socket.on('join student', (username) => {
-    const user = {
-      username,
-      id: socket.id,
-    };
-    const found = users.some((el) => el.id === socket.id);
-    if (!found) {
-      users.push(user);
-    }
-    socket.join('student');
-    io.to('instructor').emit('new user', users);
-  });
-
-  // Instructor join response
-  socket.on('join instructor', () => {
-    socket.join('instructor');
-    io.to('instructor').emit('new user', users);
-    io.to('instructor').emit('new answer', answers);
-  });
-
-  // Student send answer
-  socket.on('send answer', (payload) => {
-    const answer = {
-      ...payload,
-      id: socket.id,
-    };
-    let found = false;
-    answers.map((el) => {
-      if (el.id === socket.id) {
-        el.answer = payload.answer;
-        found = true;
-      }
+  socket.on('connect', () => {
+      console.log('client connected')
     });
-    if (!found) {
-      answers.push(answer);
-    }
-    io.to('instructor').emit('new answer', answers);
-  });
+  // Student join response
+  // socket.on('join student', (username) => {
+  //   const user = {
+  //     username,
+  //     id: socket.id,
+  //   };
+  //   const found = users.some((el) => el.id === socket.id);
+  //   if (!found) {
+  //     users.push(user);
+  //   }
+  //   socket.join('student');
+  //   io.to('instructor').emit('new user', users);
+  // });
 
-  // Instructor set timer
-  socket.on('set timer', (timer) => {
-    io.emit('start timer', timer);
-  });
+  // // Instructor join response
+  // socket.on('join instructor', () => {
+  //   socket.join('instructor');
+  //   io.to('instructor').emit('new user', users);
+  //   io.to('instructor').emit('new answer', answers);
+  // });
 
-  socket.on('disconnect', () => {
-    users = users.filter((u) => u.id !== socket.id);
-    answers = answers.filter((u) => u.id !== socket.id);
-    io.to('instructor').emit('new user', users);
-    io.to('instructor').emit('new answer', answers);
-  });
+  // // Student send answer
+  // socket.on('send answer', (payload) => {
+  //   const answer = {
+  //     ...payload,
+  //     id: socket.id,
+  //   };
+  //   let found = false;
+  //   answers.map((el) => {
+  //     if (el.id === socket.id) {
+  //       el.answer = payload.answer;
+  //       found = true;
+  //     }
+  //   });
+  //   if (!found) {
+  //     answers.push(answer);
+  //   }
+  //   io.to('instructor').emit('new answer', answers);
+  // });
+
+  // // Instructor set timer
+  // socket.on('set timer', (timer) => {
+  //   io.emit('start timer', timer);
+  // });
+
+  // socket.on('disconnect', () => {
+  //   users = users.filter((u) => u.id !== socket.id);
+  //   answers = answers.filter((u) => u.id !== socket.id);
+  //   io.to('instructor').emit('new user', users);
+  //   io.to('instructor').emit('new answer', answers);
+  // });
 });
 
 httpServer.listen(process.env.PORT || 8080, () =>
